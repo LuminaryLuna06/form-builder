de47eef7c914bee0eed7b533fa467259a9d883cf
Luna <maithetranh@gmail.com>
2025-04-23 11:51:30 +0700

    Template vite + mantine

---
+++ form-builder/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "@mantine/core/styles.css";
+
+import { MantineProvider } from "@mantine/core";
+
+function App() {
+  return (
+    <MantineProvider>
+      <div>Hello world</div>
+    </MantineProvider>
+  );
+}
+
+export default App;
