30513fac27e5bac6217df521561aaa366011beec
Luna <maithetranh@gmail.com>
2025-06-02 10:12:50 +0700

    add protectedRoute

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import FormSubmission from "../components/FormSubmission";
 import FormSubmitted from "../components/FormSubmitted";
+import LoginPage from "../components/auth/login";
+import RegisterPage from "../components/auth/register";
 
 export default function PublicRoutes() {
   return (
     <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<RegisterPage />} />
       <Route path="/form-submit/:id" element={<FormSubmission />} />
       <Route path="/form-submitted/:id" element={<FormSubmitted />} />
     </Routes>
--- src/routes/UserRoutes.tsx
+++ src/routes/UserRoutes.tsx
@@ -3,19 +3,17 @@ import PreviewForm from "../components/PreviewForm";
 import FormBuilder from "../components/FormBuilder";
 import Home from "../pages/Home/Home";
 import FormResponses from "../components/FormResponses";
-import LoginPage from "../components/auth/login";
-import RegisterPage from "../components/auth/register";
+import ProtectedRoutes from "./ProtectedRoutes";
 
 export default function UserRoutes() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<RegisterPage />} />
-      {/* <Route path="/register" element={<RegisterPage />} /> */}
-      <Route path="/" element={<Home />} />
-      <Route path="/create-form/:id" element={<FormBuilder />} />
-      <Route path="/preview/:id" element={<PreviewForm />} />
-      <Route path="/form-responses/:id" element={<FormResponses />} />
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-form/:id" element={<FormBuilder />} />
+        <Route path="/preview/:id" element={<PreviewForm />} />
+        <Route path="/form-responses/:id" element={<FormResponses />} />
+      </Route>
     </Routes>
   );
 }
