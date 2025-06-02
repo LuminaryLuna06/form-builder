0cea3ac8960b9c1052cca26ef0b2ddd795ddab3f
Luna <maithetranh@gmail.com>
2025-06-02 20:23:59 +0700

    Notification, Modals system

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,24 @@ import "@mantine/core/styles.css";
 import "@mantine/charts/styles.css";
 import "@mantine/dates/styles.css";
 import "mantine-react-table/styles.css";
+import "@mantine/notifications/styles.css";
 import "./index.css";
 
+import { Notifications } from "@mantine/notifications";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { MantineProvider } from "@mantine/core";
+import { ModalsProvider } from "@mantine/modals";
 
 import App from "./App.tsx";
-
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MantineProvider defaultColorScheme="dark">
-      <App />
+      <Notifications />
+      <ModalsProvider>
+        <App />
+      </ModalsProvider>
     </MantineProvider>
   </StrictMode>
 );
