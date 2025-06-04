a2d4d94103be7356a1008a755df249957f364608
Luna <maithetranh@gmail.com>
2025-06-04 16:49:08 +0700

    feat: add Tanstack Query and Devtools

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,23 @@ import { HashRouter as Router } from "react-router-dom";
 import PublicRoutes from "./routes/PublicRoutes";
 import UserRoutes from "./routes/UserRoutes";
 import { AuthProvider } from "./context/authContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <Router>
-      <AuthProvider>
-        <HeaderSearch />
-        <UserRoutes />
-      </AuthProvider>
-      <PublicRoutes />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <Router>
+        <AuthProvider>
+          <HeaderSearch />
+          <UserRoutes />
+        </AuthProvider>
+        <PublicRoutes />
+      </Router>
+    </QueryClientProvider>
   );
 }
 
