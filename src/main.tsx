a7b94fc8c817ccd50e8e8dc099f9ce7c65fd3948
Luna <maithetranh@gmail.com>
2025-06-04 16:46:41 +0700

    chore: delete space

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { MantineProvider } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
-
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
